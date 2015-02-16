1456b75245b30500efb65d2d60977dd4f44babd1
Adrian Lansdown <ael@thejaronline.com>
2015-02-16 22:25:16 +0000

    make server tell you the port it likes the best

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-require('harp').server(__dirname + "/dist", { port: process.env.PORT || 5000 });
+var port = process.env.PORT || 5000;
+
+console.log("The site got twenty-two hundred hits within two hours?");
+
+require('harp').server(__dirname + "/dist", { port: port });
 // so boring it hurts
+
+console.log("Server now running at http://localhost:" + port);
